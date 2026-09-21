9316a5635f89eb5b643a5652c7b4efdf36ba9469
Rutvik Makwana <rutvikmakwana4683@gmail.com>
2026-09-21 22:24:43 +0530

    Delete Models directory

--- Models/CreateCourseDto.cs
+++
@@ -1,9 +0,0 @@
-namespace prac.Models;
-
-public class CreateCourseDto
-{
-    public string CourseCode { get; set; } = string.Empty;
-    public string Title { get; set; } = string.Empty;
-    public int Credits { get; set; }
-    public int DepartmentId { get; set; }
-}
--- Models/CreateDepartmentDto.cs
+++
@@ -1,8 +0,0 @@
-namespace prac.Models;
-
-public class CreateDepartmentDto
-{
-    public string Name { get; set; } = string.Empty;
-    public string Code { get; set; } = string.Empty;
-    public string Building { get; set; } = string.Empty;
-}
